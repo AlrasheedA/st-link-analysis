c8bf0a3d9345cee50d96f8f68b9c0b3b85440746
AlrasheedA <alrasheeda29@gmail.com>
2024-06-26 15:05:32 +0300

    chore: init tests

---
+++ tests/test_smoke.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page, expect
+
+
+def test_run(page: Page):
+    expect(page).to_have_title("README")
+
+
+def test_iframe_exists(page: Page):
+    page.click("a[href*='node_style']")
+    page.wait_for_load_state("networkidle")
+    frames = page.query_selector_all("iframe[title*='st_link_analysis']")
+    assert len(frames) == 1
+
