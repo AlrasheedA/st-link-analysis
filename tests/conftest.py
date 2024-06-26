ecab899f6184c23a9bbbb59ee2548cf771174de4
AlrasheedA <33544979+AlrasheedA@users.noreply.github.com>
2024-06-26 21:26:25 +0300

    chore: init tests + add to pr workflow
    * Init tests
    * Add testing to PR workflow
    * Use Union for py 3.9 compatibility

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import time
+import pytest
+from playwright.sync_api import Page
+
+
+@pytest.fixture(autouse=True, scope="session")
+def run_streamlit():
+    import subprocess
+
+    p = subprocess.Popen(
+        [
+            "streamlit",
+            "run",
+            "app.py",
+            "--server.port",
+            "8512",
+            "--server.headless",
+            "true",
+            "--browser.gatherUsageStats",
+            "false"
+        ],
+        cwd="./examples",
+    )
+    print("hi")
+    time.sleep(3)
+    try:
+        yield 1
+    finally:
+        print("killing process")
+        p.kill()
+
+
+@pytest.fixture(autouse=True, scope="function")
+def goto_streamlit(page: Page):
+    page.goto("localhost:8512")
