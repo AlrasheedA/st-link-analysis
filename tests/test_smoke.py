0a6f293675fb2858a1d5e8d6274249bcdc79a29d
AlrasheedA <alrasheeda29@gmail.com>
2024-06-26 16:58:53 +0300

    chore(.github): add testing to PR workflow

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -10,4 +10,3 @@ def test_iframe_exists(page: Page):
     page.wait_for_load_state("networkidle")
     frames = page.query_selector_all("iframe[title*='st_link_analysis']")
     assert len(frames) == 1
-
