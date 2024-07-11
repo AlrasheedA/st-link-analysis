b30854b219aa27238697a3e4e42a45e14066f81f
AlrasheedA <alrasheeda29@gmail.com>
2024-07-11 22:01:19 +0300

    refactor(frontend): refactor toolbar and viewbar

--- st_link_analysis/frontend/src/components/viewbar.js
+++ st_link_analysis/frontend/src/components/viewbar.js
@@ -1,60 +1,70 @@
 import { debounce, getCyInstance } from "../utils/helpers";
 
 // Constants / Configurations
-const PLUS_ID = "viewbarPlus";
-const MINUS_ID = "viewbarMinus";
-const FIT_ID = "viewbarFit";
-const CENTER_ID = "viewbarCenter";
-const ZOOM_DEBOUNCE = 50;
-const FIT_DEBOUNCE = 200;
+const IDS = {
+    plus: "viewbarPlus",
+    minus: "viewbarMinus",
+    fit: "viewbarFit",
+    center: "viewbarCenter",
+};
+const DELAYS = {
+    default: 200,
+    plus: 50,
+    minus: 50,
+};
 const ZOOM_FACTOR = 0.8;
 
 // Event Handlers
-function _handlePlusClick() {
-    const cy = getCyInstance();
-    const ex = cy.extent();
-    cy.animate({
-        zoom: {
-            level: cy.zoom() * (1 / ZOOM_FACTOR),
-            position: { x: ex.x1 + 0.5 * ex.w, y: ex.y1 + 0.5 * ex.h },
-        },
-        duration: ZOOM_DEBOUNCE,
-    });
-}
+const clickHandlers = {
+    plus: debounce(() => {
+        const cy = getCyInstance();
+        const ex = cy.extent();
+        cy.animate({
+            zoom: {
+                level: cy.zoom() * (1 / ZOOM_FACTOR),
+                position: { x: ex.x1 + 0.5 * ex.w, y: ex.y1 + 0.5 * ex.h },
+            },
+            duration: DELAYS.plus,
+        });
+    }, DELAYS.plus),
 
-function _handleMinusClick() {
-    const cy = getCyInstance();
-    const ex = cy.extent();
-    cy.animate({
-        zoom: {
-            level: cy.zoom() * ZOOM_FACTOR,
-            position: { x: ex.x1 + 0.5 * ex.w, y: ex.y1 + 0.5 * ex.h },
-        },
-        duration: ZOOM_DEBOUNCE,
-    });
-}
+    minus: debounce(() => {
+        const cy = getCyInstance();
+        const ex = cy.extent();
+        cy.animate({
+            zoom: {
+                level: cy.zoom() * ZOOM_FACTOR,
+                position: { x: ex.x1 + 0.5 * ex.w, y: ex.y1 + 0.5 * ex.h },
+            },
+            duration: DELAYS.minus,
+        });
+    }, DELAYS.minus),
 
-function _handleFitClick() {
-    const cy = getCyInstance();
-    cy.animate({ fit: { padding: 20 }, duration: FIT_DEBOUNCE });
-}
+    fit: debounce(() => {
+        const cy = getCyInstance();
+        cy.animate({ fit: { padding: 20 }, duration: DELAYS.default });
+    }, DELAYS.default),
 
-function _handleCenterClick() {
-    const cy = getCyInstance();
-    cy.animate({ center: {}, duration: FIT_DEBOUNCE });
-}
+    center: debounce(() => {
+        const cy = getCyInstance();
+        cy.animate({ center: {}, duration: DELAYS.default });
+    }, DELAYS.default),
+};
 
 // View initialization
 function initViewbar() {
-    const plus = document.getElementById(PLUS_ID);
-    const minus = document.getElementById(MINUS_ID);
-    const fit = document.getElementById(FIT_ID);
-    const cent = document.getElementById(CENTER_ID);
-
-    plus.addEventListener("click", debounce(_handlePlusClick, ZOOM_DEBOUNCE));
-    minus.addEventListener("click", debounce(_handleMinusClick, ZOOM_DEBOUNCE));
-    fit.addEventListener("click", debounce(_handleFitClick, FIT_DEBOUNCE));
-    cent.addEventListener("click", debounce(_handleCenterClick, FIT_DEBOUNCE));
+    document
+        .getElementById(IDS.plus)
+        .addEventListener("click", clickHandlers.plus);
+    document
+        .getElementById(IDS.minus)
+        .addEventListener("click", clickHandlers.minus);
+    document
+        .getElementById(IDS.fit)
+        .addEventListener("click", clickHandlers.fit);
+    document
+        .getElementById(IDS.center)
+        .addEventListener("click", clickHandlers.center);
 }
 
 export default initViewbar;
