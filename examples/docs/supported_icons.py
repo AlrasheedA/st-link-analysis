0a325dff6ca58f593f52aa17141a75c60f74efe7
AlrasheedA <alrasheeda29@gmail.com>
2024-07-20 15:38:45 +0300

    style(demo): display supported icons in a streamlit dataframe

--- examples/docs/supported_icons.py
+++ examples/docs/supported_icons.py
@@ -1,37 +1,22 @@
 import streamlit as st
 from pathlib import Path
+import base64
 
 
-def prepare_svg(svg: Path) -> str:
-    return f"""\
-        <tr>
-            <td>{svg.stem}</td>
-            <td>{svg.read_text().replace("#f0f0f0", "grey")}</td>
-        </tr> """
+def prepare_icon(icon):
+    icon = icon.read_text().replace("#f0f0f0", "grey").encode("utf-8")
+    icon = base64.b64encode(icon).decode("utf-8")
+    return f"data:image/svg+xml;base64,{icon}"
 
 
 icons = Path("./../st_link_analysis/frontend/src/assets/icons").glob("*svg")
 icons = sorted(list(icons), key=lambda x: x.stem)
-icons = "".join([prepare_svg(icon) for icon in icons])
-icons = f"""\
-<html>
-    <head>
-        <style>
-            table {{
-                margin: 0 auto;
-                width: 80%;
-            }}
-        </style>
-    </head>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Icon</th>
-        </tr>
-        {icons}
-    </table>
-</html>
-"""
+icons = [{"name": icon.stem, "preview": prepare_icon(icon)} for icon in icons]
+n_icons = len(icons)
 
 st.markdown("## Supported Icons")
-st.markdown(icons, unsafe_allow_html=True)
+st.dataframe(
+    icons,
+    width=300,
+    height=(n_icons + 1) * 35 + 2,
+    column_config={"preview": st.column_config.ImageColumn()})
