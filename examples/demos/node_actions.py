fb7a0cf1cfb558d4dd9dc6ffcba3384bd60203ca
AlrasheedA <alrasheeda29@gmail.com>
2024-08-03 04:21:48 +0300

    doc(eventts&node-actions): add source code to expander as is

--- examples/demos/node_actions.py
+++ examples/demos/node_actions.py
@@ -1,6 +1,9 @@
 import json
 import streamlit as st
 from st_link_analysis import st_link_analysis, NodeStyle, EdgeStyle
+from st_link_analysis.component.layouts import LAYOUTS
+
+LAYOUT_NAMES = list(LAYOUTS.keys())
 
 st.markdown("# Expand / Remove Nodes")
 st.error(
@@ -96,7 +99,7 @@ COMPONENT_KEY = "NODE_ACTIONS"
 if not hasattr(st.session_state, "graph"):
     st.session_state.graph = DummyGraph()
 
-layout = "fcose"
+layout = st.selectbox("Try with different layouts", LAYOUT_NAMES, index=0)
 
 node_styles = [
     NodeStyle("PERSON", "#FF7F3E", "email", "person"),
@@ -132,90 +135,14 @@ with st.container(border=True):
     st.markdown("#### Returned Value")
     st.json(vals or {}, expanded=True)
 
+
+@st.cache_data
+def get_source():
+    with open(__file__, "r") as f:
+        source = f.read()
+    return source
+
+
+source = get_source()
 with st.expander("Source", expanded=False, icon="💻"):
-    st.code(
-        """
-import json
-import streamlit as st
-from st_link_analysis import st_link_analysis, NodeStyle, EdgeStyle
-
-
-class DummyGraph:
-    def __init__(self):
-        with open("./data/company.json", "r") as f:
-            elements = json.load(f)
-        self.all_nodes = elements["nodes"]
-        self.all_edges = elements["edges"]
-        self.nodes = set([n["data"]["id"] for n in elements["nodes"]])
-        self.edges = set([e["data"]["id"] for e in elements["edges"]])
-
-    def get_elements(self):
-        return {
-            "nodes": [n for n in self.all_nodes if n["data"]["id"] in self.nodes],
-            "edges": [e for e in self.all_edges if e["data"]["id"] in self.edges],
-        }
-
-    def remove(self, node_ids):
-        self.nodes -= set(node_ids)
-        self._update_edges()
-
-    def expand(self, node_ids):
-        new_nodes = set()
-        node_ids = set(node_ids)
-        for e in self.all_edges:
-            if e["data"]["source"] in node_ids:  # outbound
-                new_nodes.add(e["data"]["target"])
-            elif e["data"]["target"] in node_ids:  # inbound
-                new_nodes.add(e["data"]["source"])
-        self.nodes |= new_nodes
-        self._update_edges()
-
-    def _update_edges(self):
-        self.edges = {
-            e["data"]["id"]
-            for e in self.all_edges
-            if e["data"]["source"] in self.nodes and e["data"]["target"] in self.nodes
-        }
-
-
-COMPONENT_KEY = "NODE_ACTIONS"
-
-if not hasattr(st.session_state, "graph"):
-    st.session_state.graph = DummyGraph()
-
-layout = "fcose"
-
-node_styles = [
-    NodeStyle("PERSON", "#FF7F3E", "email", "person"),
-    NodeStyle("COMPANY", "#2A629A", "name", "business"),
-]
-
-edge_styles = [
-    EdgeStyle("WORKS_AT", labeled=True, directed=True),
-    EdgeStyle("OWNED_BY", labeled=True, directed=True),
-]
-
-
-def onchange_callback():
-    val = st.session_state[COMPONENT_KEY]
-
-    if val["action"] == "remove":
-        st.session_state.graph.remove(val["data"]["node_ids"])
-
-    elif val["action"] == "expand":
-        st.session_state.graph.expand(val["data"]["node_ids"])
-
-
-elements = st.session_state.graph.get_elements()
-
-st_link_analysis(
-    elements,
-    layout=layout,
-    node_styles=node_styles,
-    key=COMPONENT_KEY,
-    enable_node_actions=True,
-    on_change=onchange_callback,
-)
-    """,
-        language="python",
-    )
+    st.code(source, language="python")
